9b33816a127508acc4fcffe1f29c257a6aac86d1
Bernina Gray <bernina.gray@icloud.com>
2018-10-16 21:09:42 -0600

    Commit to DataDesign

--- data-design.sql
+++ data-design.sql
@@ -1,2 +1,17 @@
-DROP TABLE IF EXISTS ;
-DROP TABLE IF EXISTS ;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS article;
+
+create table category(
+	categoryId binary(16) not null,
+	castegoryName varchar(32) not null
+);
+
+create table article(
+	articleId binary (16) not null,
+	articleCategoryId varchar (255),
+	articleContent char (32),
+	articleDate varchar (128) not null,
+	articleName char(97) not null
+);
+
+
