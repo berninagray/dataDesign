adcc50a6a295c70e52114f9dff15269bfc63121e
Bernina Gray <bernina.gray@icloud.com>
2018-10-24 22:59:51 -0600

    Adding OML SCript

--- sql/omlscript.sql
+++ sql/omlscript.sql
@@ -1,4 +1,4 @@
-INSERT INTO category (categoryId, categoryName) VALUES (unhex(92ede112a7d34c988830be9b480701a8), "Politics");
+INSERT INTO category (categoryId, categoryName) VALUES (unhex(92ede112a7d34c988830be9b480701a8), "Corporations");
 
 UPDATE category set categoryName = "Politics" where categoryId = unhex("92ede112a7d34c988830be9b480701a8");
 
@@ -6,11 +6,25 @@ DELETE from category where CategoryId = unhex("92ede112a7d34c988830be9b480701a8"
 
 SELECT categoryId, categoryName where = unhex("92ede112a7d34c988830be9b480701a8");
 
-SELECT categoryId, categoryName where = categoryName = "Politics";
+SELECT categoryId, categoryName where = categoryName = "Corporations";
 
 SELECT * from category;
 
 SELECT categoryId, categoryName, from category where categoryName = unhex("92ede112a7d34c988830be9b480701a8") > min;
 
-/*Inner Joins SELECT from category*/
-SELECT
+/*Inner Joins SELECT from article*/
+
+SELECT article.articleId, article.articleCategoryId, article.articleContent, article.articleDate, article.articleName,
+category.categoryName from article innerjoin category on article.articleCategoryId = category.categoryId where articleId = uuid /*placeholder for articleId - generate one first*/
+
+/*Article insert, update, delete and select tables begin*/
+
+INSERT INTO article (articleID, articleCategoryId, articleContent, articleDate, articleName) VALUES (unhex(17c05a2ed14b44129c91536b81f94c2f) "");
+
+/*Insert with Foreign Keys*/
+
+INSERT INTO article(articleId, articleCategoryId, articleContent, articleDate, articleName) VALUES (unhex("17c05a2ed14b44129c91536b81f94c2f"), (unhex("92ede112a7d34c988830be9b480701a8"))
+
+
+UPDATE
+
