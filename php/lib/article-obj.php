37878aa6710f2c661c93de9350c7681b6892704b
Bernina Gray <bernina.gray@icloud.com>
2018-10-29 16:07:28 -0600

    Final PUSH and COMMIT

--- php/lib/article-obj.php
+++ php/lib/article-obj.php
@@ -3,4 +3,4 @@ namespace berninagray\dataDesign;
 require_once("../Classes/article.php");
 $newArticle = new article("17c05a2e-d14b-4412-9c91-536b81f94c2f", "92ede112-a7d3-4c98-8830-be9b480701a8", "foobar", "current date", "foobar");
 
-($newArticle);var_dump
+($newArticle);var_dump;
